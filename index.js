d999463694eaf814c4583e0882acb6639b8080ce
sava2026 <savchenkova.olya@gmail.com>
2026-09-03 19:57:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.get('/', async (req, res) => {
   // Устанавливаем заголовок для JSON
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
   
-  const dateStart = req.query.date_start || '2026-03-01';
-  const dateEnd = req.query.date_end || '2026-11-10';
+  const dateStart = req.query.date_start || '2026-05-01';
+  const dateEnd = req.query.date_end || '2026-10-10';
   
   console.log(`Запрос данных с ${dateStart} по ${dateEnd}`);
   
